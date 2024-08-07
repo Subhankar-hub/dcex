70a91f26aba683563257bab31d598ee765d97fa2
Subhankar Roy <subhankar.smith2003@gmail.com>
2024-08-07 17:12:20 +0530

    added callback method for checking google mail account and logging added Profilecard added swap added token list added dashboard page after login added prisma schema

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,14 +3,33 @@ import GoogleProvider from "next-auth/providers/google"
 
 //Signup // Signin with google
 
-const handler = NextAuth({
+const handler = NextAuth
+({
     
     providers: [
         GoogleProvider({
           clientId: process.env.GOOGLE_CLIENT_ID ?? "",
           clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
         })
-    ]
+    ],
+    callbacks: {
+      async signIn({ user, account, profile, email, credentials }) {
+        if (account?.provider == "google"){
+          const email = user.email;
+          if (!email) {
+            return false
+          }
+          
+        }
+        console.log({user, account, profile, email, credentials})
+        return true
+      },
+    }
 })
 
 export { handler as GET, handler as POST }
+
+console.log({
+  clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
+})
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default async function Dashboard() {
+    //const userWallet = await getUserWallet();
+
+    //if (userWallet.error || !userWallet.userWallet?.publicKey){
+        //return<>No solana wallet found</>
+    //}
+    return <div>
+        Dashboard
+    </div>
+}
