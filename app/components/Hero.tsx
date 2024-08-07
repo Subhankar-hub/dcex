08927e45876c4ea3e91d8f418556b04fa842efbd
Subhankar Roy <subhankar.smith2003@gmail.com>
2024-08-07 12:04:52 +0530

    added Hero component to page

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,9 +2,16 @@
 
 export const Hero = () => {
     return <div>
-        <div className="text-4xl font-extrabold">
-            Crypto for Indians by S.Roy
+        <div className="text-4xl font-semibold">
+            <span>Crypto for Indians by
+                </span>  
+                <span className="text-blue-500 pl-4">
+                    S.Roy
 
+                </span>
+        </div>
+        <div>
+            Create a friction less wallet with just a Google Account.
         </div>
     </div>
 }
