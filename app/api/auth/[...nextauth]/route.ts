cc306f87dc68f397dbfe1d31a52314df6ac5d814
Subhankar Roy <subhankar.smith2003@gmail.com>
2024-08-13 11:23:50 +0530

    database index

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
+import db from "@/app/db";
+import { Keypair } from "@solana/web3.js";
 
 //Signup // Signin with google
 
@@ -19,6 +21,41 @@ const handler = NextAuth
           if (!email) {
             return false
           }
+          const userDb = await db.user.findFirst({
+            where: {
+              username: email
+            }
+          })
+
+          if (userDb){
+            return true;
+          }
+          const keypair = Keypair.generate();
+          const publicKey = keypair.publicKey.toBase58();
+          const privateKey = keypair.secretKey;
+          console.log(publicKey)
+          console.log(privateKey)
+
+          await db.user.create({
+            data: {
+              username: email,
+              name: "",
+              profilePicture: "m",
+              provider: "Google",
+              solWallet:{
+                create: {
+                  publicKey: publicKey,
+                  privateKey: privateKey.toString()
+                }
+              },
+              inrWallet:{
+                create:{
+                  balance: 0
+                }
+              }
+            }
+          })
+          return true;
           
         }
         console.log({user, account, profile, email, credentials})
